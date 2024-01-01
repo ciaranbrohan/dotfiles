0fec9346e14cacc5bbbb13d5b7a01defe2a752c4
Ciaran <ciaran.brohan@gmail.com>
2024-01-01 15:59:53 +0000

    added alpha homedash, added completions and formating

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,9 +5,10 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
+        auto_install = true,
         ensure_installed = {"lua", "javascript"},
         highlight = { enable = true },
-        indent = { enable = true },  
+        indent = { enable = true },
       })
     end
   }
